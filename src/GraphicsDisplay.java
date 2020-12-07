2952ad2923f628aaa0caac527cfbd37668e4220e
volha <volhav_l@mail.ru>
2020-12-07 21:20:11 +0300

    Commit4

--- src/GraphicsDisplay.java
+++ src/GraphicsDisplay.java
@@ -44,16 +44,12 @@ public class GraphicsDisplay extends JPanel{
         // Шрифт для подписей осей координат
         axisFont = new Font("Serif", Font.BOLD, 16);
     }
-    // Данный метод вызывается из обработчика элемента меню "Открыть файл с рафиком"
-    // главного окна приложения в случае успешной загрузки данных
     public void showGraphics(Double[][] graphicsData) {
         // Сохранить массив точек во внутреннем поле класса
         this.graphicsData = graphicsData;
         // Запросить перерисовку компонента, т.е. неявно вызвать paintComponent();
         repaint();
     }
-    // Методы-модификаторы для изменения параметров отображения графика
-    // Изменение любого параметра приводит к перерисовке области
     public void setShowAxis(boolean showAxis) {
         this.showAxis = showAxis;
         repaint();
@@ -64,21 +60,12 @@ public class GraphicsDisplay extends JPanel{
     }
     // Метод отображения всего компонента, содержащего график
     public void paintComponent(Graphics g) {
-        /* Шаг 1 - Вызвать метод предка для заливки области цветом заднего фона
-         * Эта функциональность - единственное, что осталось в наследство от
-         * paintComponent класса JPanel
-         */
         super.paintComponent(g);
-        // Шаг 2 - Если данные графика не загружены (при показе компонента при запуске программы) - ничего не делать
         if (graphicsData == null || graphicsData.length == 0) return;
-        // Шаг 3 - Определить минимальное и максимальное значения для координат X и Y
-        // Это необходимо для определения области пространства, подлежащей отображению
-        // Еѐ верхний левый угол это (minX, maxY) - правый нижний это (maxX, minY)
         minX = graphicsData[0][0];
         maxX = graphicsData[graphicsData.length - 1][0];
         minY = graphicsData[0][1];
         maxY = minY;
-        // Найти минимальное и максимальное значение функции
         for (int i = 1; i < graphicsData.length; i++) {
             if (graphicsData[i][1] < minY) {
                 minY = graphicsData[i][1];
@@ -87,14 +74,8 @@ public class GraphicsDisplay extends JPanel{
                 maxY = graphicsData[i][1];
             }
         }
-/* Шаг 4 - Определить (исходя из размеров окна) масштабы по осям X
-и Y - сколько пикселов
-* приходится на единицу длины по X и по Y
-*/
         double scaleX = getSize().getWidth() / (maxX - minX);
         double scaleY = getSize().getHeight() / (maxY - minY);
-// Шаг 5 - Чтобы изображение было неискажѐнным - масштаб должен быть одинаков
-// Выбираем за основу минимальный
         scale = Math.min(scaleX, scaleY);
         if (scale == scaleX) {
             double yIncrement = (getSize().getHeight() / scale - (maxY - minY)) / 2;
@@ -102,7 +83,6 @@ public class GraphicsDisplay extends JPanel{
             minY -= yIncrement;
         }
         if (scale == scaleY) {
-// Если за основу был взят масштаб по оси Y, действовать по аналогии
             double xIncrement = (getSize().getWidth() / scale - (maxX -
                     minX)) / 2;
             maxX += xIncrement;
@@ -121,26 +101,19 @@ public class GraphicsDisplay extends JPanel{
         canvas.setColor(oldColor);
         canvas.setStroke(oldStroke);
     }
-    // Отрисовка графика по прочитанным координатам
     protected void paintGraphics(Graphics2D canvas) {
-// Выбрать линию для рисования графика
         canvas.setStroke(graphicsStroke);
-// Выбрать цвет линии
         canvas.setColor(Color.ORANGE);
         GeneralPath graphics = new GeneralPath();
         for (int i=0; i<graphicsData.length; i++) {
-// Преобразовать значения (x,y) в точку на экране point
             Point2D.Double point = xyToPoint(graphicsData[i][0],
                     graphicsData[i][1]);
             if (i>0) {
-// Не первая итерация цикла - вести линию в точку point
                 graphics.lineTo(point.getX(), point.getY());
             } else {
-// Первая итерация цикла - установить начало пути в точку point
                 graphics.moveTo(point.getX(), point.getY());
             }
         }
-// Отобразить график
         canvas.draw(graphics);
     }
     protected void paintMarkers(Graphics2D canvas) {
@@ -177,7 +150,6 @@ public class GraphicsDisplay extends JPanel{
             Point2D.Double corner = shiftPoint(center, 3, 3);
         }
     }
-    // Метод, обеспечивающий отображение осей координат
     protected void paintAxis(Graphics2D canvas) {
 // Установить особое начертание для осей
         canvas.setStroke(axisStroke);
@@ -189,11 +161,7 @@ public class GraphicsDisplay extends JPanel{
         canvas.setFont(axisFont);
 // Создать объект контекста отображения текста - для получения характеристик устройства (экрана)
                 FontRenderContext context = canvas.getFontRenderContext();
-// Определить, должна ли быть видна ось Y на графике
         if (minX<=0.0 && maxX>=0.0) {
-// Она должна быть видна, если левая граница показываемой области (minX) <= 0.0,
-            // а правая (maxX) >= 0.0
-// Сама ось - это линия между точками (0, maxY) и (0, minY)
             canvas.draw(new Line2D.Double(xyToPoint(0, maxY),
                     xyToPoint(0, minY)));
 // Стрелка оси Y
@@ -207,23 +175,15 @@ public class GraphicsDisplay extends JPanel{
 // Вести нижнюю часть стрелки в точку с относительными координатами (-10, 0)
             arrow.lineTo(arrow.getCurrentPoint().getX()-10,
                     arrow.getCurrentPoint().getY());
-// Замкнуть треугольник стрелки
             arrow.closePath();
             canvas.draw(arrow); // Нарисовать стрелку
             canvas.fill(arrow);
-            // Закрасить стрелку
-// Нарисовать подпись к оси Y
-// Определить, сколько места понадобится для надписи "y"
             Rectangle2D bounds = axisFont.getStringBounds("y", context);
             Point2D.Double labelPos = xyToPoint(0, maxY);
-// Вывести надпись в точке с вычисленными координатами
             canvas.drawString("y", (float)labelPos.getX() + 10,
                     (float)(labelPos.getY() - bounds.getY()));
         }
-// Определить, должна ли быть видна ось X на графике
         if (minY<=0.0 && maxY>=0.0) {
-// Она должна быть видна, если верхняя граница показываемой области (maxX) >= 0.0,
-// а нижняя (minY) <= 0.0
                     canvas.draw(new Line2D.Double(xyToPoint(minX, 0), xyToPoint(maxX, 0)));
 // Стрелка оси X
             GeneralPath arrow = new GeneralPath();
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -17,37 +17,26 @@ import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
 public class MainFrame extends JFrame{
-        // Начальные размеры окна приложения
-        private static final int WIDTH = 800;
-        private static final int HEIGHT = 600;
-// Объект диалогового окна для выбора файлов
-private JFileChooser fileChooser = null;
-    // Пункты меню
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private JFileChooser fileChooser = null;
+
     private JCheckBoxMenuItem showAxisMenuItem;
     private JCheckBoxMenuItem showMarkersMenuItem;
-    // Компонент-отображатель графика
     private GraphicsDisplay display = new GraphicsDisplay();
-    // Флаг, указывающий на загруженность данных графика
     private boolean fileLoaded = false;
     public MainFrame() {
-        // Вызов конструктора предка Frame
         super("Построение графиков функций на основе заранее подготовленных файлов");
-// Установка размеров окна
                 setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
-// Отцентрировать окно приложения на экране
         setLocation((kit.getScreenSize().width - WIDTH)/2,
                 (kit.getScreenSize().height - HEIGHT)/2);
-// Развѐртывание окна на весь экран
         setExtendedState(MAXIMIZED_BOTH);
-// Создать и установить полосу меню
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         menuBar.setBackground(Color.ORANGE);
-// Добавить пункт меню "Файл"
         JMenu fileMenu = new JMenu("Файл");
         menuBar.add(fileMenu);
-// Создать действие по открытию файла
         Action openGraphicsAction = new AbstractAction("Открыть файл с графиком") {
         public void actionPerformed(ActionEvent event) {
             if (fileChooser==null) {
